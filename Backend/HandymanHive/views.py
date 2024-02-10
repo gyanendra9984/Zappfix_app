27b918c3bc3abcb90a590452730327d21a47ff8f
Prashant <prashantsingh2226@gmail.com>
2024-02-10 12:20:40 +0530

    Database integration

--- Backend/HandymanHive/views.py
+++ Backend/HandymanHive/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+#-------------------- USER AUTHENTICATION --------------
+@csrf_exempt
+def signup(request):
+    return JsonResponse({"message": "Signup successfull"})
+
+
+@csrf_exempt
+def login(request):
+    return JsonResponse({"message": "Login successfull"})   
+    
+
