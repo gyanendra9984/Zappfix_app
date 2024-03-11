5cc19ea2de966c5f8ef42e6b456f1b50adcfc7df
gyanendra9984 <mani15102002@gmail.com>
2024-03-11 21:42:13 +0530

    user model created

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,20 +22,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oqsy04&_h+59j_u=oqv91efx3azfm@&#(+fkd#0h*6gc6k6wzv'
+SECRET_KEY = "django-insecure-oqsy04&_h+59j_u=oqv91efx3azfm@&#(+fkd#0h*6gc6k6wzv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Replace with your preferred backend
+EMAIL_BACKEND = (
+    "django.core.mail.backends.smtp.EmailBackend"  # Replace with your preferred backend
+)
 
-EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_PORT = 587  
-EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = os.getenv("Email")  
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("Email")
 EMAIL_HOST_PASSWORD = os.getenv("Email_Password")
 
 
@@ -44,53 +45,52 @@ EMAIL_HOST_PASSWORD = os.getenv("Email_Password")
 
 INSTALLED_APPS = [
     # 'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'HandymanHive',
-    'corsheaders'
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "HandymanHive",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
-AUTH_USER_MODEL = 'HandymanHive.CustomWorker'
+AUTH_USER_MODEL = "HandymanHive.CustomWorker"
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-    
+    "django.contrib.auth.backends.ModelBackend",
 ]
 
-ROOT_URLCONF = 'Backend.urls'
+ROOT_URLCONF = "Backend.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'Backend.wsgi.application'
+WSGI_APPLICATION = "Backend.wsgi.application"
 
 
 # Database
@@ -113,16 +113,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -130,9 +130,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -142,12 +142,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Corsheaders settings
--- Backend/HandymanHive/models.py
+++ Backend/HandymanHive/models.py
@@ -11,7 +11,7 @@ class OTPModel(models.Model):
 
     def __str__(self):
         return self.email
-    
+
 class CustomWorkerManager(BaseUserManager):
     def create_user(self, email, **extra_fields):
         if not email:
@@ -62,7 +62,7 @@ class CustomWorker(AbstractBaseUser, PermissionsMixin):
 class Service(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, help_text="Description of the service")   
-    
+
 
 class Certification(models.Model):    
     name = models.CharField(max_length=255, help_text="Name of the certification")
@@ -74,16 +74,16 @@ class Location(models.Model):
     neighborhood = models.CharField(max_length=50, blank=True, null=True, help_text="Neighborhood within the city")
     street_address = models.CharField(max_length=255, blank=True, null=True, help_text="Street address within the neighborhood")
     zip_code = models.CharField(max_length=10, blank=True, null=True, help_text="ZIP code")
-    
-    
+
+
 # Model to store professional details of workers.
 class WorkerDetails(models.Model): 
     worker = models.OneToOneField(CustomWorker, on_delete=models.CASCADE)
-    
+
     # Services and Skills
     services_offered = models.ManyToManyField(Service, related_name='workers')
     skill_level = models.IntegerField()
-    
+
     isAvailable = models.BooleanField(default=True)        
 
     # Work Experience
@@ -105,12 +105,50 @@ class WorkerDetails(models.Model):
     min_price = models.DecimalField(max_digits=10, decimal_places=2)
     max_price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    
-    
-  
 
- 
-    
-    
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, **extra_fields):
+        if not email:
+            raise ValueError(_("The Email field must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_unusable_password()
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        user = self.create_user(email, **extra_fields)
+        user.save()
+        return user
 
 
+# Model to store user profile.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(primary_key=True, max_length=255, unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    otp = models.CharField(max_length=6, null=True, blank=True)
+    otp_valid_till = models.DateTimeField(null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "gender"]
+
+    def __str__(self):
+        return self.email
