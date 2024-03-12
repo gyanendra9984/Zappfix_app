3a71f2b26d9e1ab47aaf6270843f44fff3fe0407
Prashant <prashantsingh2226@gmail.com>
2024-03-12 19:00:02 +0530

    Edit User profile

--- Backend/HandymanHive/urls.py
+++ Backend/HandymanHive/urls.py
@@ -4,17 +4,18 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("worker_signup", views.worker_signup, name="worker_signup"),
-    path("user_signup", views.worker_signup, name="user_signup"),
-    path("worker_login", views.worker_login, name="worker_login"),
     path("verify_otp", views.verify_otp, name="verify_otp"),
-    path("verify_user_otp", views.verify_otp, name="verify_user_otp"),
-    path("delete_entry", views.delete_entry, name="delete_entry"),
+    path("worker_login", views.worker_login, name="worker_login"),
     path("verify_login_otp", views.verify_login_otp, name="verify_login_otp"),
     path(
         "edit_worker_personal_profile",
         views.edit_worker_personal_profile,
         name="edit_worker_personal_profile",
     ),
-  path('worker_delete', views.worker_delete, name='worker_delete'),
+    path('worker_delete', views.worker_delete, name='worker_delete'),
+    path("user_signup", views.worker_signup, name="user_signup"),    
+    path("verify_user_otp", views.verify_otp, name="verify_user_otp"), 
+    path("edit_user_profile",views.edit_user_profile, name="edit_user_profile"),    
+    
 ]  
 
