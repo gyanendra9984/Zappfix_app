e29b96f2f8bc9f6542abd7bb1fe4d4b2653ecdbb
AJtheDeMoN <ajayathwal911@gmail.com>
2024-03-12 20:39:12 +0530

    add edit option for profile

--- zappfix/navigation/AppNav.js
+++ zappfix/navigation/AppNav.js
@@ -20,7 +20,8 @@ const AppNav = () => {
     }
     return (
       <NavigationContainer>
-        { userToken!=null ? <AppStack/>:<AuthStack/>}
+        { userToken!=null ? <AppStack/>:<AppStack/>}
+        {/*{ userToken!=null ? <AppStack/>:<AuthStack/>}*/}
       </NavigationContainer>
     );
 }
