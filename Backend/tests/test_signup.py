e181fef9d62ebfafefdf13ef2c58e1783924de7f
Abhishek <jaiswalabhi0786@gmail.com>
2024-03-23 19:57:57 +0530

    signup_report

--- Backend/tests/test_signup.py
+++ Backend/tests/test_signup.py
@@ -7,37 +7,45 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 @pytest.mark.django_db
-def test_signup():
-    client = Client()
-    valid_data={
-        "email": "test@example.com",
-        "isWorker": False,
-    }
-    response = client.post(reverse('user_signup'), json.dumps(valid_data), content_type='application/json')
-    assert response.status_code == 200
-    assert "OTP sent successfully" in response.json().get("message", "")
+class TestSignup(TestCase):
 
-    duplicate_data={
-        "email": "test@example.com",
-        "isWorker": False,
-    }
-    response = client.post(reverse('user_signup'), json.dumps(duplicate_data), content_type='application/json')
-    assert response.status_code == 400
-    assert "User already exists" in response.json().get("message", "")
+    def test_valid_signup(self):
+        client = Client()
+        valid_data = {
+            "email": "test@example.com",
+            "isWorker": False,
+        }
+        response = client.post(reverse('user_signup'), json.dumps(valid_data), content_type='application/json')
+        assert response.status_code == 200
+        assert "OTP sent successfully" in response.json().get("message", "")
 
-    invalid_data={
-        "email": "test",
-        "isWorker": False,
-    }
-    response = client.post(reverse('user_signup'), json.dumps(invalid_data), content_type='application/json')
-    assert response.status_code == 400
-    assert "Invalid email" in response.json().get("message", "")
+    def test_duplicate_signup(self):
+        client = Client()
+        duplicate_data = {
+            "email": "test@example.com",
+            "isWorker": False,
+        }
+        response = client.post(reverse('user_signup'), json.dumps(duplicate_data), content_type='application/json')
+        assert response.status_code == 400
+        assert "User already exists" in response.json().get("message", "")
 
-    invalid_data={
-        "email": "",
-        "isWorker": False,
-    }
-    response = client.post(reverse('user_signup'), json.dumps(invalid_data), content_type='application/json')
-    assert response.status_code == 400
-    assert "Invalid email" in response.json().get("message", "")
+    def test_invalid_email_signup(self):
+        client = Client()
+        invalid_data = {
+            "email": "test",
+            "isWorker": False,
+        }
+        response = client.post(reverse('user_signup'), json.dumps(invalid_data), content_type='application/json')
+        assert response.status_code == 400
+        assert "Invalid email" in response.json().get("message", "")
+
+    def test_empty_email_signup(self):
+        client = Client()
+        invalid_data = {
+            "email": "",
+            "isWorker": False,
+        }
+        response = client.post(reverse('user_signup'), json.dumps(invalid_data), content_type='application/json')
+        assert response.status_code == 400
+        assert "Invalid email" in response.json().get("message", "")
 
